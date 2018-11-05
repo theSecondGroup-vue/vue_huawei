517b5e96d3a47d01f34d6a8fca9362ea59da9706
koala <470360294@qq.com>
2018-11-05 20:04:56 +0800

    公告样式提交

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -12,6 +12,7 @@ export default {
 </script>
 
 <style>
+@import url(../static/css/public.css);
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
